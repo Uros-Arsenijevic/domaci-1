448cd449db676cc38f2db956f890736f07caffc4
Uros Arsenijevic <142681439+Uros-Arsenijevic@users.noreply.github.com>
2023-09-06 17:20:01 +0200

    Domaci 1

---
+++ domaci.js
@@ -0,0 +1,8 @@
+let compjuter = {
+    procesor: "Rizen 7",
+    "Maticna ploca": "Asus 4040D",
+};
+
+compjuter.Ram = "8gb";
+
+console.log(compjuter)
---
+++ vezba.js
@@ -0,0 +1,21 @@
+// let computer = ["Maticna ploca", "Procesor", "SSD"];
+
+// computer[2] = "HDD";
+
+// console.log(computer);
+
+// computer.push("Kuciste");
+
+// console.log(computer)
+
+// Object 
+
+// let computer = {
+//     SSD: 3000,
+//     "Hard disk": 4000,
+//     "Maticna ploca": 7000,
+//     Procesor: 12_0000,
+//     "Graficka karta": 100_000,
+//     "Na trzistu": true,
+//     Napomena: "Komjuter dolazi bez operativnog sistema",
+// };
